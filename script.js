bea3c87768f803e453b32d944cbae9caab1621b6
sid6145 <sidtdesh@gmail.com>
2021-08-25 22:35:02 +0530

    completed with website

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const burger = document.querySelector("#burger")
 const nav = document.querySelector(".nav-items")
+const search = document.querySelector(".search-input")
 
 burger.addEventListener('click', () => {
     nav.classList.toggle("nav-activate")
 })
 
+search.addEventListener('focusin', () => {
+    search.classList.add('expand')
+})
+
+search.addEventListener('focusout', () => {
+    search.classList.remove('expand')
+})
